b19e9f55e582d0ff7dbc377a63a968d6bb687c58
thomcr <thomcr@29190a3a-a094-b4a7-7311-dbdd62bc676b>
2012-10-16 04:23:47 +0000

    .

--- apps/frontend/modules/principal/templates/seguridadSuccess.php
+++ apps/frontend/modules/principal/templates/seguridadSuccess.php
@@ -1,52 +1,6 @@
 	<section id="content">
-		<article class="col1">
-			<div class="pad_1">
-				<h2>Your Flight Planner</h2>
-				<form id="form_1" action="" method="post">
-					<div class="wrapper pad_bot1">
-						<div class="radio marg_right1">
-							<input type="radio" name="name1">Round Trip<br>
-							<input type="radio" name="name1">One Way
-						</div>
-						<div class="radio">
-							<input type="radio" name="name1">Empty-Leg<br>
-							<input type="radio" name="name1">Multi-Leg
-						</div>
-					</div>
-					<div class="wrapper">
-						Leaving From:
-						<div class="bg"><input type="text" class="input input1" value="Enter City or Airport Code" onBlur="if(this.value=='') this.value='Enter City or Airport Code'" onFocus="if(this.value =='Enter City or Airport Code' ) this.value=''"></div>
-					</div>
-					<div class="wrapper">
-						Going To:
-						<div class="bg"><input type="text" class="input input1" value="Enter City or Airport Code" onBlur="if(this.value=='') this.value='Enter City or Airport Code'" onFocus="if(this.value =='Enter City or Airport Code' ) this.value=''"></div>
-					</div>
-					<div class="wrapper">
-						Departure Date and Time:
-						<div class="wrapper">
-							<div class="bg left"><input type="text" class="input input2" value="mm/dd/yyyy " onBlur="if(this.value=='') this.value='mm/dd/yyyy '" onFocus="if(this.value =='mm/dd/yyyy ' ) this.value=''"></div>
-							<div class="bg right"><input type="text" class="input input2" value="12:00am" onBlur="if(this.value=='') this.value='12:00am'" onFocus="if(this.value =='12:00am' ) this.value=''"></div>
-						</div>
-					</div>
-					<div class="wrapper">
-						Return Date and Time:
-						<div class="wrapper">
-							<div class="bg left"><input type="text" class="input input2" value="mm/dd/yyyy " onBlur="if(this.value=='') this.value='mm/dd/yyyy '" onFocus="if(this.value =='mm/dd/yyyy ' ) this.value=''"></div>
-							<div class="bg right"><input type="text" class="input input2" value="12:00am" onBlur="if(this.value=='') this.value='12:00am'" onFocus="if(this.value =='12:00am' ) this.value=''"></div>
-						</div>
-					</div>
-					<div class="wrapper">
-						<p>Passenger(s):</p>
-						<div class="bg left"><input type="text" class="input input2" value="# passengers" onBlur="if(this.value=='') this.value='# passengers'" onFocus="if(this.value =='# passengers' ) this.value=''"></div>
-						<a href="#" class="button2" onClick="document.getElementById('form_1').submit()">go!</a>
-					</div>
-				</form>
-				<h2>Safety Experts</h2>
-				<p><strong>Temporibus autem</strong> quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis.</p>
-				<div class="wrapper pad_bot2"><a href="#" class="button2">Read More</a></div>
-			</div>
-		</article>
-		<article class="col2 pad_left1">
+		
+		<article class="col2 pad_left1_1">
 			<h2>About Safety</h2>
 			<div class="wrapper">
 				<figure class="left marg_right1"><img src="images/page3_img1.jpg" alt=""></figure>
